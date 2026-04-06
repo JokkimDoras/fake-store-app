1d82ed3455e819d69a8d166b13cf11a4cf739ef2
Jokkim <jokkimxavier@gmail.com>
2026-04-06 21:26:28 +0530

    some css Stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Fragment,useContext } from "react"
 import { DataContext } from "./context/DataContext"
-
+import NavBar from "./component/NavBar"
 export default function App () {
 
     const {data} = useContext(DataContext)
@@ -8,17 +8,27 @@ export default function App () {
 
     
     const renderedStuff = data.map((det) => {
-        return <div key={det.id}>
+        return <div className="header-items" key={det.id}>
+
+<div className="items-stuff">
+                
             <div>
       <img  src={det.image}/>
             </div>
 
             <h4>{det.title}</h4>
 
+</div>
         </div>
     })
 
-    return <Fragment>
+    return   (<div>
+        <NavBar/>
+
+         <div className="for-flex">
+
+
        {renderedStuff}
-    </Fragment>
+        </div>
+    </div>)
 }
--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
     return (
-        <div>
+        <div className="nav-header">
             NavBar
         </div>
     )
